108a02e1fdf0f8a188620cc1060140d9df8a195c
Kunal Punjabi <kunalspunjabi@gmail.com>
2023-07-19 14:17:52 -0400

    cleanup

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use KeenBrain\Billable;
 
 class User extends Authenticatable
 {
-    use Billable, HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
